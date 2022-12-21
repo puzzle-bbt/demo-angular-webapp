4214f3b25026de4ac4f80f25baec1188e3ee95f5
Lias Kleisa <kleisa@puzzle.ch>
2022-12-21 16:43:43 +0100

    Implement method for gray background and no vertical lines

--- src/app/chart/linechart/linechart.component.ts
+++ src/app/chart/linechart/linechart.component.ts
@@ -1,5 +1,7 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {Chart, registerables} from 'chart.js/auto';
+import {Chart, ChartItem, registerables} from 'chart.js/auto';
+import _default from "chart.js/dist/plugins/plugin.tooltip";
+import backgroundColor = _default.defaults.backgroundColor;
 Chart.register(...registerables);
 
 
@@ -17,16 +19,30 @@ export class LinechartComponent implements OnInit {
 
   ngOnInit(): void {
 
+    const plugin = {
+      id: 'chart_area_background_color',
+      beforeDraw: (chart: { width?: any; height?: any; ctx?: any; }, args: any, options: { color: any; }) => {
+        const {ctx} = chart;
+        ctx.save();
+        ctx.globalCompositeOperation = 'destination-over';
+        ctx.fillStyle = options.color;
+        ctx.fillRect(27, 10, chart.width - 35, chart.height - 20);
+        ctx.restore();
+      },
+      defaults: {
+        color: 'lightgray'
+      }
+    }
 
-
+    const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
     new Chart("myChart", {
       type: 'line',
       data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        labels: labels,
         datasets: [{
           data: [12, 19, 3, 5, 2, 3],
           pointStyle: 'circle',
-          pointRadius:10 ,
+          pointRadius:5 ,
           backgroundColor: ['black'],
           borderColor: ['black'],
           borderWidth: 2,
@@ -34,15 +50,22 @@ export class LinechartComponent implements OnInit {
         }]
       },
       options: {
-        responsive: true,
+        backgroundColor: '#FE242422',
         animation: false,
-        plugins: {legend:{display: false}, tooltip:{enabled: true}},
+        plugins: {legend:{display: false}, tooltip:{enabled: true}, chart_area_background_color: {}},
+
         scales: {
           y: {
-            beginAtZero: true
+            beginAtZero: true,
+          },
+          x: {
+            display: false
           }
-        }
-      }
+        },
+        responsive: true,
+
+      },
+      plugins: [plugin],
     });
   }
 
