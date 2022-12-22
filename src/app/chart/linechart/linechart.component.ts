58a91648ff8c1a9709fd4ee337cc1e83170240bc
peggimann <peggimann@puzzle.com>
2022-12-22 11:46:41 +0100

    implement date as x-axis and enable irregular distances on x-axis

--- src/app/chart/linechart/linechart.component.ts
+++ src/app/chart/linechart/linechart.component.ts
@@ -1,7 +1,8 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {Chart, ChartItem, registerables} from 'chart.js/auto';
 import _default from "chart.js/dist/plugins/plugin.tooltip";
-import backgroundColor = _default.defaults.backgroundColor;
+import 'chartjs-adapter-moment'
+
 Chart.register(...registerables);
 
 
@@ -26,7 +27,7 @@ export class LinechartComponent implements OnInit {
         ctx.save();
         ctx.globalCompositeOperation = 'destination-over';
         ctx.fillStyle = options.color;
-        ctx.fillRect(27, 10, chart.width - 35, chart.height - 20);
+        ctx.fillRect(27, 10, chart.width - 39, chart.height - 39);
         ctx.restore();
       },
       defaults: {
@@ -34,36 +35,59 @@ export class LinechartComponent implements OnInit {
       }
     }
 
-    const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+    const labels = ['Jan', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     new Chart("myChart", {
-      type: 'line',
+      type: 'scatter',
       data: {
-        labels: labels,
+        // labels: labels,
         datasets: [{
-          data: [12, 19, 3, 5, 2, 3],
-          pointStyle: 'circle',
-          pointRadius:5 ,
           backgroundColor: ['black'],
           borderColor: ['black'],
           borderWidth: 2,
+          data: [
+          { x: '2020/09/02', y: 15 },
+          { x: '2020/11/03', y: 16},
+          { x: '2020/11/04', y: 19},
+          { x: '2020/11/07', y: 22},
+          { x: '2020/11/08', y: 25},
+          { x: '2020/12/09', y: 25},
+
+
+        ],
+          pointRadius: 5,
+          pointStyle: 'circle',
 
         }]
       },
       options: {
+        showLine: true,
         backgroundColor: '#FE242422',
         animation: false,
-        plugins: {legend:{display: false}, tooltip:{enabled: true}, chart_area_background_color: {}},
+        spanGaps: true,
+        plugins: {legend: {display: false}, tooltip: {enabled: true}, chart_area_background_color: {}},
 
         scales: {
-          y: {
-            beginAtZero: true,
-          },
           x: {
-            display: false
-          }
+            type: 'time',
+            // min: Date.now() - (24 * 60 * 60 * 1000),
+            // max: Date.now() + (24 * 60 * 60 * 1000),
+            ticks: {
+              source: 'auto',
+            },
+            time: {
+              unit: 'day', //second
+              displayFormats: {
+                hour: "dd/MM/yyyy" //second = 'dd/MM/yyyy HH:mm:ss'
+              },
+              tooltipFormat: "dd/MM/yyyy",
+            },
+          },
+          y: {
+            min: 0,
+
+          },
         },
         responsive: true,
-
       },
       plugins: [plugin],
     });
